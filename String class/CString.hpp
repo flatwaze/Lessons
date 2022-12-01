3f3ba6cd7fafa9334fb2a9115ea4a4d46218e5eb
flatwaze <37955552+flatwaze@users.noreply.github.com>
2022-12-01 12:30:10 +0300

    Create CString.hpp

---
+++ String class/CString.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+#define LEN 128
+
+class CString{
+    private:
+        char *str;
+        int length;
+
+    public:
+        CString();
+        CString(char other[]);
+        CString(const CString& other);
+
+        int size(){
+            return length;
+        }
+
+        const CString operator+(const CString& other) const;
+        const CString& operator=(const CString& other);
+
+        char& operator[](int i);
+        friend std :: ostream& operator<<(std :: ostream &out, const CString& s);
+        friend std :: istream& operator>>(std :: istream &out, CString& s);
+};
