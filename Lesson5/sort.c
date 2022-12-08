98975b546dc49e3086b4eabfb2e8c92f4968004a
flatwaze <37955552+flatwaze@users.noreply.github.com>
2022-12-08 18:23:57 +0300

    Create sort.c

---
+++ Lesson5/sort.c
@@ -0,0 +1,75 @@
+void merge(int* arr, int left, int right){
+    int i, j, k, mid, n1, n2;
+
+    mid = (left + right - 1)/2;
+
+    n1 = mid - left + 1;
+    n2 = right - mid;
+
+    int* L = (int*)malloc(sizeof(int) * n1);
+    int* R = (int*)malloc(sizeof(int) * n2);
+
+    for (i = 0; i < n1; i++) L[i] = arr[left + i];
+	for (i = 0; i < n2; i++) R[i] = arr[mid + 1 + i]; 
+
+
+    i = 0; j = 0; k = left;
+    
+    
+	while (i < n1 && j < n2) {
+		if (L[i] <= R[j]) {
+			arr[k] = L[i];
+			i++;
+		}
+		else {
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+    while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+
+    free(L); free(R);
+}
+
+void mergeSort(int* arr, int left, int right){
+    int mid = (left + right - 1)/2;
+    if(left < right){
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid + 1, right);
+        merge(arr, left, right);
+    }
+}
+
+
+int binarySearch(double elem, double *arr, int len){
+    int left, mid, right;
+
+    left = 0;
+    right = len - 1;
+
+    int index = -1;
+
+    while(right - left > 0){
+        mid = (left + right)/2;
+        if(elem == arr[mid]){
+            index = mid;
+            break;
+        }
+        else if(elem > arr[mid]) left = mid + 1;
+        else right = mid - 1;
+    }
+
+    return index;
+}
