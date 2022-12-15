4e3441e57ad41584a880eb70e37c90d88a4e4337
flatwaze <37955552+flatwaze@users.noreply.github.com>
2022-12-15 13:19:07 +0300

    Create 11.c

---
+++ Lesson6/11.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 100
+
+void reverse(int* array, int len){
+    int tmp;
+    for(int i = 0; i < len/2; i++){
+        tmp = array[i];
+        array[i] = array[len - i - 1];
+        array[len - i - 1] = tmp;
+    }
+}
+
+int main(void){
+    int len, shift, num, q;
+    int arr[MAX];
+    FILE *in, *out;
+
+    in = fopen("data/input.txt", "r");
+    out = fopen("data/output.txt", "w");
+
+    len = 0;
+
+    while((q = fscanf(in, "%d", &num)) == 1){
+        arr[len] = num;
+        len++;
+    }
+    if(q == 0) return -1; /*Invalid data*/
+
+    printf("Enter the shift count: ");
+    scanf("%d", &shift);
+
+    shift %= len;
+
+    reverse(arr, len - shift);
+    reverse(arr + len - shift, shift);
+    reverse(arr, len);
+
+    for(int i = 0; i < len; i++) fprintf(out, "%d ", arr[i]);
+
+    return 0;
+}
